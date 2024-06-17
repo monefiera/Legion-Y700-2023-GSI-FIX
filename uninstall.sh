234a89fe4665bd97e1f7e721664c0e89ed33135b
MONE FIERA <83483662+monefiera@users.noreply.github.com>
2024-06-18 04:32:10 +0900

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -17,3 +17,27 @@
   done < "$INFO"
   rm -rf "$INFO"
 }
+pm enable-user --user 0 com.android.providers.contacts
+pm enable-user --user 0 com.android.nfc
+pm enable-user --user 0 com.android.smspush
+pm enable-user --user 0 me.phh.treble.overlay.cafims_telephony
+pm enable-user --user 0 me.phh.treble.overlay.hwims_telephony
+pm enable-user --user 0 me.phh.treble.overlay.mtkims_telephony
+pm enable-user --user 0 me.phh.treble.overlay.slsiims_telephony
+pm enable-user --user 0 me.phh.treble.overlay.sprdims_telephony
+pm enable-user --user 0 me.phh.treble.overlay.telephony.lte
+pm enable-user --user 0 me.phh.treble.overlay.tethering
+pm enable-user --user 0 com.android.mms.service
+pm enable-user --user 0 com.android.stk
+pm enable-user --user 0 com.android.simappdialog
+pm enable-user --user 0 com.android.providers.blockednumber
+pm enable-user --user 0 com.android.messaging
+pm enable-user --user 0 com.android.cellbroadcastreceiver.module
+pm enable-user --user 0 com.android.carrierdefaultapp
+pm enable-user --user 0 com.android.server.telecom
+pm enable-user --user 0 com.android.dialer
+pm enable-user --user 0 com.android.phone
+pm enable-user --user 0 com.android.contacts
+pm enable-user --user 0 im.angry.openeuicc
+pm enable-user --user 0 com.google.android.dialer
+pm enable-user --user 0 com.google.android.contacts
